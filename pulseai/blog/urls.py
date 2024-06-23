7272f3226e9b797daf9ae00412c3bb56a365831e
Antaryami Sahoo <mail2sahooantaryami@gmail.com>
2024-06-23 10:43:54 +0530

    contact and about url added

--- pulseai/blog/urls.py
+++ pulseai/blog/urls.py
@@ -3,6 +3,6 @@ from blog import views
 
 urlpatterns = [
       path('', views.index, name="home"),
-#     path('about/', views.about,name="about"),
-#     path('contact/', views.contact,name="contact")
+      path('about/', views.about,name="about"),
+      path('contact/', views.contact,name="contact")
 ]
--- pulseai/blog/views.py
+++ pulseai/blog/views.py
@@ -4,3 +4,35 @@ from django.shortcuts import render, HttpResponse # type: ignore
 def index(request):
       # return HttpResponse("Hello World!!!")
       return render(request,'blog/index.html')
+
+def about(request):
+      # return HttpResponse("<h1>About Us</h1>")
+      return render(request,'blog/about.html')
+
+
+def contact(request):
+      mobile=8926194183
+      email="er.antaryami@gmail.com"
+      a=10
+      b=10
+      languages=['C','C++','Java','Python']
+      blog={
+            "titile":"Blog Title",
+            "auhor":"John",
+            "content":"Blog details"
+      }
+      students=[
+            {"roll":1,"name":"sibu","cgpa":9.2},
+            {"roll":2,"name":"lipu","cgpa":8.7},
+            
+      ]
+      context={
+            "mob":mobile,
+            "email":email,
+            "a":a,
+            "b":b,
+            "languages":languages,
+            'blog':blog,
+            'students':students,
+      }
+      return render(request,'blog/contact.html',context)
