bdc7c606d398c2f900d436937355fb447ab6de5d
Antaryami Sahoo <mail2sahooantaryami@gmail.com>
2024-06-23 12:48:19 +0530

    admin created, Blog and Blog_category model added & media folder created

--- pulseai/blog/admin.py
+++ pulseai/blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from blog.models import BlogCategory, Blog
 # Register your models here.
+admin.site.register(BlogCategory)
+admin.site.register(Blog)
--- pulseai/pulseai/urls.py
+++ pulseai/pulseai/urls.py
@@ -24,3 +24,6 @@ urlpatterns = [
     # path('patient/', include('patient.urls')),
     path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
