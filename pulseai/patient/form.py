58758f7401377518f760ccdea0ce2af59b7cd4e3
Antaryami Sahoo <mail2sahooantaryami@gmail.com>
2024-06-24 14:37:37 +0530

    Signup, signin and signout Form added

---
+++ pulseai/patient/form.py
@@ -0,0 +1,11 @@
+from django.forms import ModelForm
+
+
+from blog.models import Blog
+
+class BlogForm(ModelForm):
+	class Meta:
+		model = Blog
+		# fields = '__all__'
+		fields = ['title','content']
+
--- pulseai/pulseai/settings.py
+++ pulseai/pulseai/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'django_summernote',
     'blog',
-    # 'patient',
+    'patient',
 ]
 
 MIDDLEWARE = [
